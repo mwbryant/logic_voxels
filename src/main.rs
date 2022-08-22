4d34fc3fdee6710fe764cc345fdbbca27d656167
Matthew <logicprojectsforfpgas@gmail.com>
2022-08-22 10:20:38 -0600

    Organization

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use std::{
-    f32::consts::PI,
-    ops::{Index, IndexMut},
-};
+use std::ops::{Index, IndexMut};
 
-use bevy::{
-    prelude::*,
-    render::{mesh::Indices, render_resource::PrimitiveTopology},
-    window::PresentMode,
-};
+use bevy::{prelude::*, window::PresentMode};
 use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 use bevy_inspector_egui::WorldInspectorPlugin;
+use chunk_mesh_generation::create_chunk_mesh;
 use noise::{NoiseFn, Perlin};
+
+mod chunk_mesh_generation;
 
 #[derive(Component)]
 pub struct FollowCamera;
@@ -70,13 +66,9 @@ fn main() {
         .add_plugin(NoCameraPlayerPlugin)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_custom_mesh)
-        .add_system(rotator)
         .add_system(camera_follow)
         .run();
 }
-
-#[derive(Component)]
-struct Rotator;
 
 fn camera_follow(
     camera: Query<&Transform, With<Camera3d>>,
@@ -85,191 +77,6 @@ fn camera_follow(
     for mut follower in &mut followers {
         follower.translation = camera.single().translation;
     }
-}
-
-fn rotator(mut to_rotate: Query<&mut Transform, With<Rotator>>, time: Res<Time>) {
-    for mut transform in &mut to_rotate {
-        transform.rotate_axis(Vec3::Y, time.delta_seconds());
-    }
-}
-
-fn add_face(
-    vertices: &mut Vec<Vec3>,
-    normals: &mut Vec<Vec3>,
-    indicies: &mut Vec<usize>,
-    rotation: Quat,
-    transform: Vec3,
-) {
-    let mut new_verts = [
-        Vec3::new(-BLOCK_SIZE / 2.0, -BLOCK_SIZE / 2.0, 0.0),
-        Vec3::new(BLOCK_SIZE / 2.0, -BLOCK_SIZE / 2.0, 0.0),
-        Vec3::new(BLOCK_SIZE / 2.0, BLOCK_SIZE / 2.0, 0.0),
-        Vec3::new(-BLOCK_SIZE / 2.0, BLOCK_SIZE / 2.0, 0.0),
-    ];
-    let mut new_normals = [
-        Vec3::new(0.0, 0.0, 1.0),
-        Vec3::new(0.0, 0.0, 1.0),
-        Vec3::new(0.0, 0.0, 1.0),
-        Vec3::new(0.0, 0.0, 1.0),
-    ];
-
-    new_verts
-        .iter_mut()
-        .for_each(|vec| *vec = (rotation * *vec) + transform);
-    new_normals.iter_mut().for_each(|vec| *vec = rotation * *vec);
-    //info!("{}", new_normals[1]);
-
-    let vert_start = vertices.len();
-    vertices.extend_from_slice(&new_verts);
-    normals.extend_from_slice(&new_normals);
-
-    indicies.extend_from_slice(&[vert_start, vert_start + 1, vert_start + 2]);
-    indicies.extend_from_slice(&[vert_start, vert_start + 2, vert_start + 3]);
-}
-
-//Clippy is angry but I am going to add more to the if clauses soon and the suggestions are less clear
-#[allow(clippy::nonminimal_bool)]
-fn create_mesh_faces(
-    chunk: &Chunk,
-    chunk_neighbors: [Option<&Chunk>; 6],
-    verts: &mut Vec<Vec3>,
-    normals: &mut Vec<Vec3>,
-    indicies: &mut Vec<usize>,
-) {
-    for z in 0..CHUNK_SIZE {
-        for y in 0..CHUNK_SIZE {
-            for x in 0..CHUNK_SIZE {
-                //Front
-                if (x != CHUNK_SIZE - 1 && chunk.cubes[x][y][z] && !chunk.cubes[x + 1][y][z])
-                    || (x == CHUNK_SIZE - 1
-                        && chunk.cubes[x][y][z]
-                        && (chunk_neighbors[ChunkDirection::Front].is_none()
-                            || !chunk_neighbors[ChunkDirection::Front].unwrap().cubes[0][y][z]))
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::Y, PI / 2.0),
-                        Vec3::new(
-                            (x as f32 + 0.5) * BLOCK_SIZE,
-                            y as f32 * BLOCK_SIZE,
-                            (z as f32 - 0.5) * BLOCK_SIZE,
-                        ),
-                    );
-                }
-                //Back
-                if (x != 0 && chunk.cubes[x][y][z] && !chunk.cubes[x - 1][y][z])
-                    || (x == 0 && chunk.cubes[x][y][z])
-                        && (chunk_neighbors[ChunkDirection::Back].is_none()
-                            || !chunk_neighbors[ChunkDirection::Back].unwrap().cubes[CHUNK_SIZE - 1][y][z])
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::Y, -PI / 2.0),
-                        Vec3::new(
-                            (x as f32 - 0.5) * BLOCK_SIZE,
-                            y as f32 * BLOCK_SIZE,
-                            (z as f32 - 0.5) * BLOCK_SIZE,
-                        ),
-                    );
-                }
-                //Top
-                //TODO Y neighbors are untested
-                if (y != CHUNK_SIZE - 1 && chunk.cubes[x][y][z] && !chunk.cubes[x][y + 1][z])
-                    || (y == CHUNK_SIZE - 1 && chunk.cubes[x][y][z])
-                        && (chunk_neighbors[ChunkDirection::Top].is_none()
-                            || !chunk_neighbors[ChunkDirection::Top].unwrap().cubes[x][0][z])
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::X, -PI / 2.0),
-                        Vec3::new(
-                            x as f32 * BLOCK_SIZE,
-                            (y as f32 + 0.5) * BLOCK_SIZE,
-                            (z as f32 - 0.5) * BLOCK_SIZE,
-                        ),
-                    );
-                }
-                //Bottom
-                if (y != 0 && chunk.cubes[x][y][z] && !chunk.cubes[x][y - 1][z])
-                    || (y == 0 && chunk.cubes[x][y][z])
-                        && (chunk_neighbors[ChunkDirection::Bottom].is_none()
-                            || !chunk_neighbors[ChunkDirection::Bottom].unwrap().cubes[x][CHUNK_SIZE - 1][z])
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::X, PI / 2.0),
-                        Vec3::new(
-                            x as f32 * BLOCK_SIZE,
-                            (y as f32 - 0.5) * BLOCK_SIZE,
-                            (z as f32 - 0.5) * BLOCK_SIZE,
-                        ),
-                    );
-                }
-                //Left
-                if (z != CHUNK_SIZE - 1 && chunk.cubes[x][y][z] && !chunk.cubes[x][y][z + 1])
-                    || (z == CHUNK_SIZE - 1 && chunk.cubes[x][y][z])
-                        && (chunk_neighbors[ChunkDirection::Left].is_none()
-                            || !chunk_neighbors[ChunkDirection::Left].unwrap().cubes[x][y][0])
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::Y, 0.0),
-                        Vec3::new((x as f32) * BLOCK_SIZE, y as f32 * BLOCK_SIZE, (z as f32) * BLOCK_SIZE),
-                    );
-                }
-                //Right
-                if (z != 0 && chunk.cubes[x][y][z] && !chunk.cubes[x][y][z - 1])
-                    || (z == 0 && chunk.cubes[x][y][z])
-                        && (chunk_neighbors[ChunkDirection::Right].is_none()
-                            || !chunk_neighbors[ChunkDirection::Right].unwrap().cubes[x][y][CHUNK_SIZE - 1])
-                {
-                    add_face(
-                        verts,
-                        normals,
-                        indicies,
-                        Quat::from_axis_angle(Vec3::Y, PI),
-                        Vec3::new(
-                            (x as f32) * BLOCK_SIZE,
-                            y as f32 * BLOCK_SIZE,
-                            (z as f32 - 1.0) * BLOCK_SIZE,
-                        ),
-                    );
-                }
-            }
-        }
-    }
-}
-
-fn create_chunk_mesh(chunk: &Chunk, neighbors: [Option<&Chunk>; 6]) -> Mesh {
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let mut verts = Vec::default();
-    let mut normals = Vec::default();
-    let mut indicies = Vec::default();
-
-    create_mesh_faces(chunk, neighbors, &mut verts, &mut normals, &mut indicies);
-
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        verts.iter().map(|vec| vec.to_array()).collect::<Vec<[f32; 3]>>(),
-    );
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        normals.iter().map(|vec| vec.to_array()).collect::<Vec<[f32; 3]>>(),
-    );
-    mesh.set_indices(Some(Indices::U32(
-        indicies.iter().map(|usized| *usized as u32).collect::<Vec<u32>>(),
-    )));
-    mesh
 }
 
 fn gen_chunk(chunk_x: f32, chunk_z: f32) -> Chunk {
