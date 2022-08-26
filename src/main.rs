42653668ebf6e047dd1b6556c5b284aa15c44229
Matthew <logicprojectsforfpgas@gmail.com>
2022-08-26 15:42:25 -0600

    deadlock :(

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ mod material;
 pub struct FollowCamera;
 
 pub const CHUNK_SIZE: usize = 16;
-pub const WORLD_SIZE: usize = 100;
+pub const WORLD_SIZE: usize = 10;
 pub const MAX_CHUNK_UPDATES_PER_FRAME: usize = 10;
 pub const BLOCK_SIZE: f32 = 0.5;
 
@@ -51,7 +51,7 @@ impl<T> IndexMut<ChunkDirection> for [T; 6] {
     }
 }
 
-type ChunkData = RwLock<[[[Block; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE]>;
+type ChunkData = [[[Block; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE];
 #[derive(Component, Clone)]
 pub struct ChunkComp {
     chunk: Arc<RwLock<Chunk>>,
@@ -59,47 +59,47 @@ pub struct ChunkComp {
 
 //TODO serialize?
 //PERF there has to be a more performant way of handling this
-//#[derive(Component, Clone)]
+#[derive(Clone)]
 pub struct Chunk {
-    cubes: ChunkData>,
-    dirty: RwLock<bool>>,
-    neighbors: [Weak<ChunkData>; 6],
+    cubes: ChunkData,
+    dirty: bool,
+    neighbors: [Weak<RwLock<Chunk>>; 6],
 }
 
 impl Chunk {
     pub fn get_block(&self, x: isize, y: isize, z: isize) -> Option<Block> {
         if Self::index_inbounds(x) && Self::index_inbounds(y) && Self::index_inbounds(z) {
-            Some(self.cubes.clone().read().unwrap()[x as usize][y as usize][z as usize])
+            Some(self.cubes[x as usize][y as usize][z as usize])
         } else if x < 0 {
             assert!(Self::index_inbounds(y) && Self::index_inbounds(z));
             self.neighbors[ChunkDirection::Back]
                 .upgrade()
-                .map(|back| back.read().unwrap()[CHUNK_SIZE - 1][y as usize][z as usize])
+                .map(|back| back.read().unwrap().cubes[CHUNK_SIZE - 1][y as usize][z as usize])
         } else if x >= CHUNK_SIZE as isize {
             assert!(Self::index_inbounds(y) && Self::index_inbounds(z));
             self.neighbors[ChunkDirection::Front]
                 .upgrade()
-                .map(|front| front.read().unwrap()[0][y as usize][z as usize])
+                .map(|front| front.read().unwrap().cubes[0][y as usize][z as usize])
         } else if z < 0 {
             assert!(Self::index_inbounds(x) && Self::index_inbounds(y));
             self.neighbors[ChunkDirection::Right]
                 .upgrade()
-                .map(|front| front.read().unwrap()[x as usize][y as usize][CHUNK_SIZE - 1])
+                .map(|front| front.read().unwrap().cubes[x as usize][y as usize][CHUNK_SIZE - 1])
         } else if z >= CHUNK_SIZE as isize {
             assert!(Self::index_inbounds(x) && Self::index_inbounds(y));
             self.neighbors[ChunkDirection::Left]
                 .upgrade()
-                .map(|back| back.read().unwrap()[x as usize][y as usize][0])
+                .map(|back| back.read().unwrap().cubes[x as usize][y as usize][0])
         } else if y < 0 {
             assert!(Self::index_inbounds(x) && Self::index_inbounds(z));
             self.neighbors[ChunkDirection::Bottom]
                 .upgrade()
-                .map(|bottom| bottom.read().unwrap()[x as usize][CHUNK_SIZE - 1][z as usize])
+                .map(|bottom| bottom.read().unwrap().cubes[x as usize][CHUNK_SIZE - 1][z as usize])
         } else if y >= CHUNK_SIZE as isize {
             assert!(Self::index_inbounds(x) && Self::index_inbounds(z));
             self.neighbors[ChunkDirection::Top]
                 .upgrade()
-                .map(|top| top.read().unwrap()[x as usize][0][z as usize])
+                .map(|top| top.read().unwrap().cubes[x as usize][0][z as usize])
         } else {
             None
         }
@@ -151,8 +151,8 @@ pub enum Block {
 impl Default for Chunk {
     fn default() -> Chunk {
         Chunk {
-            cubes: Arc::new(RwLock::new([[[Block::Air; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE])),
-            dirty: Arc::new(RwLock::new(false)),
+            cubes: [[[Block::Air; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE],
+            dirty: true,
             neighbors: [
                 Weak::new(),
                 Weak::new(),
@@ -165,26 +165,27 @@ impl Default for Chunk {
     }
 }
 
-fn update_dirty_chunks(mut chunks: Query<(&Chunk, &mut Handle<Mesh>)>, mut meshes: ResMut<Assets<Mesh>>) {
+fn update_dirty_chunks(mut chunks: Query<(&ChunkComp, &mut Handle<Mesh>)>, mut meshes: ResMut<Assets<Mesh>>) {
     //TODO all of this can be done in parallel except for adding mesh to assets
     //FIXME for now I'm just going to cap the number of chunk updates per frame
     let mut updates = 0;
     for (chunk, mut mesh) in &mut chunks {
-        if *chunk.dirty.read().unwrap() {
-            *mesh = meshes.add(create_chunk_mesh(chunk));
+        if chunk.chunk.read().unwrap().dirty {
+            *mesh = meshes.add(create_chunk_mesh(&chunk.chunk.read().unwrap()));
             updates += 1;
-            *chunk.dirty.write().unwrap() = false;
+            chunk.chunk.write().unwrap().dirty = false;
         }
         if updates > MAX_CHUNK_UPDATES_PER_FRAME {
+            info!("too many dirty chunks");
             return;
         }
     }
 }
 
-fn update_dirt_sys(chunks: Query<&Chunk>, input: Res<Input<KeyCode>>) {
+fn update_dirt_sys(chunks: Query<&ChunkComp>, input: Res<Input<KeyCode>>) {
     if input.just_pressed(KeyCode::Space) {
         chunks.par_for_each(5, |chunk| {
-            apply_function_to_blocks(chunk, update_dirt);
+            apply_function_to_blocks(&mut chunk.chunk.write().unwrap(), update_dirt);
         });
     }
 }
@@ -216,7 +217,7 @@ fn main() {
         .run();
 }
 
-fn apply_function_to_blocks<F>(chunk: &Chunk, mut function: F)
+fn apply_function_to_blocks<F>(chunk: &mut Chunk, mut function: F)
 where
     F: FnMut(&mut Block, [Option<Block>; 6]) -> bool,
 {
@@ -224,11 +225,8 @@ where
         for y in 0..CHUNK_SIZE as isize {
             for x in 0..CHUNK_SIZE as isize {
                 let neighbors = chunk.get_block_neighbors(x, y, z);
-                if function(
-                    &mut chunk.cubes.clone().write().unwrap()[x as usize][y as usize][z as usize],
-                    neighbors,
-                ) {
-                    *chunk.dirty.write().unwrap() = true;
+                if function(&mut chunk.cubes[x as usize][y as usize][z as usize], neighbors) {
+                    chunk.dirty = true;
                 }
             }
         }
@@ -257,7 +255,7 @@ fn camera_follow(
 }
 
 fn gen_chunk(chunk_x: f32, chunk_z: f32) -> Chunk {
-    let chunk = Chunk::default();
+    let mut chunk = Chunk::default();
     let perlin = Perlin::new();
 
     for z in 0..CHUNK_SIZE {
@@ -275,7 +273,7 @@ fn gen_chunk(chunk_x: f32, chunk_z: f32) -> Chunk {
                         ])
                         + 0.06);
                 if value >= (y as f32 / CHUNK_SIZE as f32) as f64 || y == 0 {
-                    chunk.cubes.clone().write().unwrap()[x][y][z] = Block::Grass
+                    chunk.cubes[x][y][z] = Block::Grass
                 }
             }
         }
@@ -303,58 +301,56 @@ fn spawn_custom_mesh(
             chunks[x].push(chunk);
         }
     }
+
+    //let mut arcs: [[Weak<_>; WORLD_SIZE]; WORLD_SIZE] = Default::default();
+    let mut arcs = [(); WORLD_SIZE].map(|_| [(); WORLD_SIZE].map(|_| <Arc<_>>::default()));
+    let mut weaks = [(); WORLD_SIZE].map(|_| [(); WORLD_SIZE].map(|_| <Weak<_>>::default()));
+    //Create arcs
+    for x in 0..chunks_to_spawn {
+        for z in 0..chunks_to_spawn {
+            let arc = Arc::new(RwLock::new(chunks[x][z].clone()));
+            arcs[x][z] = arc;
+            weaks[x][z] = Arc::downgrade(&arcs[x][z]);
+        }
+    }
+
     //link chunk neighbors
     for x in 0..chunks_to_spawn {
         for z in 0..chunks_to_spawn {
             if x != chunks_to_spawn - 1 {
-                chunks[x][z].neighbors[ChunkDirection::Front] = Arc::downgrade(&chunks[x + 1][z].cubes);
+                arcs[x][z].clone().write().unwrap().neighbors[ChunkDirection::Front] = weaks[x + 1][z].clone();
             }
             if x != 0 {
-                chunks[x][z].neighbors[ChunkDirection::Back] = Arc::downgrade(&chunks[x - 1][z].cubes);
+                arcs[x][z].clone().write().unwrap().neighbors[ChunkDirection::Back] = weaks[x - 1][z].clone();
             }
             if z != 0 {
-                chunks[x][z].neighbors[ChunkDirection::Right] = Arc::downgrade(&chunks[x][z - 1].cubes);
+                arcs[x][z].clone().write().unwrap().neighbors[ChunkDirection::Right] = weaks[x][z - 1].clone();
             }
             if z != chunks_to_spawn - 1 {
-                chunks[x][z].neighbors[ChunkDirection::Left] = Arc::downgrade(&chunks[x][z + 1].cubes);
+                arcs[x][z].clone().write().unwrap().neighbors[ChunkDirection::Left] = weaks[x][z + 1].clone();
             }
         }
     }
 
-    let mesh_descs: Vec<(Mesh, f32, f32, Chunk)> = (0..chunks_to_spawn)
-        .into_par_iter()
-        .map(|x| {
-            (0..chunks_to_spawn)
-                .into_iter()
-                .map(|z| {
-                    let chunk_x = x as f32 * CHUNK_SIZE as f32 * BLOCK_SIZE;
-                    let chunk_z = z as f32 * CHUNK_SIZE as f32 * BLOCK_SIZE;
-                    //TESTING
-                    {
-                        let _span = info_span!("span_name", name = "span_name").entered();
-                        //apply_function_to_blocks(&chunks[x][z], update_dirt);
-                    }
+    for x in 0..chunks_to_spawn {
+        for z in 0..chunks_to_spawn {
+            let chunk_x = x as f32 * CHUNK_SIZE as f32 * BLOCK_SIZE;
+            let chunk_z = z as f32 * CHUNK_SIZE as f32 * BLOCK_SIZE;
+            commands
+                .spawn_bundle(MaterialMeshBundle {
+                    mesh: meshes.add(create_chunk_mesh(&chunks[x][z])),
+                    //mesh: meshes.add(shape::Box::default().into()),
+                    material: materials.add(CustomMaterial {
+                        textures: server.load("test_texture.png"),
+                    }),
+                    transform: Transform::from_xyz(chunk_x, 0.0, chunk_z),
 
-                    let _span = info_span!("create_mesh", name = "create_mesh").entered();
-                    (create_chunk_mesh(&chunks[x][z]), chunk_x, chunk_z, chunks[x][z].clone())
+                    ..default()
                 })
-                .collect::<Vec<_>>()
-        })
-        .flatten()
-        .collect();
-
-    for mesh_desc in mesh_descs {
-        commands
-            .spawn_bundle(MaterialMeshBundle {
-                mesh: meshes.add(mesh_desc.0),
-                material: materials.add(CustomMaterial {
-                    textures: server.load("test_texture.png"),
-                }),
-                transform: Transform::from_xyz(mesh_desc.1, 0.0, mesh_desc.2),
-
-                ..default()
-            })
-            .insert(mesh_desc.3);
+                .insert(ChunkComp {
+                    chunk: arcs[x][z].clone(),
+                });
+        }
     }
 }
 
